f83a618740ec1602b48c2619b547f03ee5f12cb7
Sravya <gokadasravya@gmail.com>
2026-07-21 15:41:07 +0530

    Fix Router duplication error in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import RatingPage from './pages/RatingPage';
 import UserManagementPage from './pages/UserManagementPage';
 import NotificationPage from './pages/NotificationPage';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<RatingPage />} />
-        <Route path="/user-management" element={<UserManagementPage />} />
-        <Route path="/notification" element={<NotificationPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<RatingPage />} />
+      <Route path="/user-management" element={<UserManagementPage />} />
+      <Route path="/notification" element={<NotificationPage />} />
+    </Routes>
   );
 }
 
